cc89a7edfa039fff29b4f29195de3191000896d5
denilsonbezerra <denilsonbezerra.10@hotmail.com>
2022-02-12 22:16:33 -0300

    Upando para o GitHub

---
+++ src/js/painel.js
@@ -0,0 +1,33 @@
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+const totalDeImagens = imagensPainel.length - 1;
+let imagemAtual = 0;
+
+function esconderImagens() {
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar');
+    });
+}
+
+function mostrarImagem() {
+    imagensPainel[imagemAtual].classList.add('mostrar');
+}
+
+setaAvancar.addEventListener('click', function () {
+    if(imagemAtual === totalDeImagens) {
+        return;
+    }
+    imagemAtual++;
+    esconderImagens();
+    mostrarImagem();
+})
+
+setaVoltar.addEventListener('click', function () {
+    if(imagemAtual === 0) {
+        return;
+    }
+    imagemAtual--;
+    esconderImagens();
+    mostrarImagem();
+})
